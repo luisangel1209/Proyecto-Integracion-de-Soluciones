bfec82480d253f9a44a0e472f9e5665159915044
maria <maria@DESKTOP-K1BUDO1>
2021-01-12 16:36:20 -0600

    controller de formulario agrega cita

--- FitNutrition/src/fitnutrition/FXMLFormularioAgregaCitaController.java
+++ FitNutrition/src/fitnutrition/FXMLFormularioAgregaCitaController.java
@@ -14,6 +14,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -45,6 +46,8 @@ public class FXMLFormularioAgregaCitaController implements Initializable, Notifi
     private NotificaCambios notificacion;
     private boolean isNuevo;
     private Citas cita;
+    @FXML
+    private Button botonGuardar;
     
 
     /**
