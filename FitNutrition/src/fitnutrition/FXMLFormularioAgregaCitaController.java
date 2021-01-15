ec8b85b04f5111c5e251443a51fc59473325c6cb
maria <maria@DESKTOP-K1BUDO1>
2021-01-14 22:19:22 -0600

    clicEnviar agregado

--- FitNutrition/src/fitnutrition/FXMLFormularioAgregaCitaController.java
+++ FitNutrition/src/fitnutrition/FXMLFormularioAgregaCitaController.java
@@ -5,6 +5,7 @@ import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import java.lang.reflect.Type;
 import java.net.URL;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
@@ -70,8 +71,10 @@ public class FXMLFormularioAgregaCitaController implements Initializable, Notifi
     
     private void cargaDatosEdicion(){
         if(cita != null){
-            //idPaciente.setText(cita.getIdPaciente());
-            //dpFechaCita.(cita.getFechaCita());
+            idPaciente.setText(cita.getIdPaciente().toString());
+            String fecha = cita.getFecha_Cita();
+            LocalDate fecha2 = LocalDate.parse(fecha);
+            dpFechaCita.setValue(fecha2);
             tfHoraCita.setText(cita.getHora_Cita());
             tfObservaciones.setText(cita.getObservaciones());
         }
@@ -79,7 +82,60 @@ public class FXMLFormularioAgregaCitaController implements Initializable, Notifi
     
     @FXML 
     private void clicEnviar (ActionEvent e){
-        
+        if(isNuevo){
+            String url = Constantes.URL + "fitNuutrition/registraCita";
+            int idPacientee = Integer.parseInt(idPaciente.getText());
+            String date = ""+dpFechaCita.getValue();
+            String parametros = String.format("idPaciente=%s&fechaCita=%s&horaCita=%s&observaciones=%s", 
+                    idPacientee,
+                    date,
+                    tfHoraCita.getText(),
+                    tfObservaciones.getText()
+                    );
+            
+            RespuestaWS resp = ConsumoWS.consumoWSPOST(url, parametros);
+            if(resp.getCodigo() == 200){
+                Gson gson = new Gson();
+                Mensaje msj = gson.fromJson(resp.getMensaje(), Mensaje.class);
+                if(msj.isError()){
+                    DialogError("Error al agregar", msj.getMensaje());
+                }else{
+                    muestraDialogo("Registro Agregado", msj.getMensaje());
+                    CerrarScena();
+                }
+            }else{
+                DialogError("Error de conexión", "Lo sentimos, tenemos problemas para conectar con el servidor");
+            }
+        }else{
+            String url = Constantes.URL + "fitNuutrition/editarCita";
+            int idPacientee = Integer.parseInt(idPaciente.getText());
+            String date = ""+dpFechaCita.getValue();
+            String parametros = String.format("idPaciente=%s&fechaCita=%s&horaCita=%s&observaciones=%s", 
+                    idPacientee,
+                    date,
+                    tfHoraCita.getText(),
+                    tfObservaciones.getText()                    
+                    );
+            RespuestaWS res = ConsumoWS.consumoWSPUT(url, parametros);
+            if(res.getCodigo() == 200){
+                Gson gson = new Gson();
+                Mensaje msj = gson.fromJson(res.getMensaje(), Mensaje.class);
+                if(msj.isError()){
+                    DialogError("Error al editar", msj.getMensaje());
+                }else{
+                    muestraDialogo("Registro editado", msj.getMensaje());
+                    CerrarScena();
+                }
+            }else{
+                DialogError("Error de conexión", "Lo sentimos, tenemos problemas para conectar con el servidor");
+            }
+        }
+    }
+    
+    private void CerrarScena(){
+        Stage stage = (Stage) this.botonGuardar.getScene().getWindow();
+        stage.close();
+        notificacion.RefrescarTlaba(true);
     }
     
     private void muestraDialogo(String titulo, String mensaje){
@@ -98,7 +154,8 @@ public class FXMLFormularioAgregaCitaController implements Initializable, Notifi
         error.showAndWait();
     }
     
-    //error en override
+    
+    @Override
     public void RefrescarTlaba(boolean dato){
         throw new UnsupportedOperationException("Not supported yet.");
     }
